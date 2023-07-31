8b9ff83fa80a3e95d6a55e79beb8955b727ac7e8
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-07-31 23:21:36 +0300

    123

--- src/components/pages/MoviesPage/MoviesPage.jsx
+++ src/components/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, } from 'react';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 import { MoviesList } from 'components/MoviesList/MoviesList';
 import { getByQuerry } from 'Api/fetchByQuerry';
